3759726394977276c04c5a7d803b1244f8ceba1c
difer10 <dfclaros@unicauca.edu.co>
2023-11-25 20:31:47 -0500

    modificacion del settings y algunos detalles

--- webempresarial/settings.py
+++ webempresarial/settings.py
@@ -82,13 +82,24 @@ WSGI_APPLICATION = 'webempresarial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
+# con postgre
 
+DATABASES = {
+    'default': {
+        'ENGINE': config('ENGINE'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
